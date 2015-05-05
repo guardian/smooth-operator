7f9bfd7014305cd6ce162fc71dfab1dfdeabc75c
proberts <paul.roberts@guardian.co.uk>
2015-05-05 17:49:35 +0100

    fix a problem with not getting any results from schedule

--- app/lib/pagerduty/PagerDutyAPI.scala
+++ app/lib/pagerduty/PagerDutyAPI.scala
@@ -10,30 +10,37 @@ import scala.concurrent.{ExecutionContext, Future}
 import JsonImplicits._
 
 class PagerDutyAPI(val authToken: String, val baseUrl: String) extends PagerDutyApiUtils {
-/**
-  *  https://developer.pagerduty.com/documentation/rest/schedules/users
-  */
-def whoIsOn(scheduleId: String, startDate: DateTime, endDate: DateTime)(implicit ec: ExecutionContext):
-    Future[Either[String, Seq[User]]] = {
-  val future =
-    submitJson(s"schedules/$scheduleId/users",
-               "since" -> startDate.toString,
-               "until" -> endDate.toString)
-  future map { response =>
-    response.right.flatMap { json =>
-      (json \ "users").validate[Seq[User]] match {
-        case JsSuccess(users, _) => Right(users)
-        case err: JsError     => Left(err.toString)
+  /**
+    *  https://developer.pagerduty.com/documentation/rest/schedules/users
+    */
+  def whoIsOn(scheduleId: String, startDate: DateTime, endDate: DateTime)(implicit ec: ExecutionContext):
+      Future[Either[String, Seq[User]]] = {
+    val future =
+      submitJson(s"schedules/$scheduleId/users",
+                 "since" -> startDate.toString,
+                 "until" -> endDate.toString)
+
+    // parse the JSON into a sequence of Users
+    future map { response =>
+      response.right.flatMap { json =>
+        (json \ "users").validate[Seq[User]] match {
+          case JsSuccess(users, _) => Right(users)
+          case err: JsError        => Left(err.toString)
+        }
       }
     }
   }
-}
 
   /**
     * default to today
     */
-  def whoIsOn(scheduleId: String)(implicit ec: ExecutionContext): Future[Either[String, Seq[User]]] =
-    whoIsOn(scheduleId, new DateTime, new DateTime)
+  def whoIsOn(scheduleId: String)(implicit ec: ExecutionContext): Future[Either[String, Seq[User]]] = {
+    val now   = new DateTime
+    val start = now
+    // if start and end are the same, you don't any results back ... ?
+    val end   = start.plusMinutes(1)
+    whoIsOn(scheduleId, start, end)
+  }
 }
 
 object PagerDutyAPI {
