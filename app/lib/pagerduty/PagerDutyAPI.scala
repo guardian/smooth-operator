be7741ba4df98d7485d16eb011fde17fc4b68550
proberts <paul.roberts@guardian.co.uk>
2015-05-05 17:23:40 +0100

    some refactoring

--- app/lib/pagerduty/PagerDutyAPI.scala
+++ app/lib/pagerduty/PagerDutyAPI.scala
@@ -1,42 +1,15 @@
 package lib.pagerduty
 
 import java.net.URI
-import play.api.libs.ws.{WSResponse, WS}
-import play.api.libs.json._
 import play.api.Play.{current, configuration}
+import play.api.libs.json._
 import play.api.http.Status
 import org.joda.time.DateTime
 import scala.concurrent.{ExecutionContext, Future}
 
 import JsonImplicits._
 
-class PagerDutyAPI(authToken: String, base: String, apiVersion: String = "v1") extends Status {
-  require(base.startsWith("https"), "Please don't use Pager Duty without HTTPS")
-
-  private def authHeader = ("Authorization" -> s"Token token=$authToken")
-
-  private def request(path: String) = WS.url(s"$base/api/$apiVersion/$path").withHeaders(authHeader)
-
-  private def submit(path: String, params: (String, String)*)(implicit ec: ExecutionContext) =
-    request(path).withQueryString(params: _*).execute
-
-  private def submitJson(path: String, params: (String, String)*)(implicit ec: ExecutionContext):
-      Future[Either[String, JsValue]] =
-    submit(path, params: _*).map { response =>
-      if(response.status == OK) Right(response.json) else Left(getError(response.json))
-    }
-
-  /**
-    *  checks whether the WSResponse that was returned by PagerDuty was
-    *  an error (see:
-    *  https://developer.pagerduty.com/documentation/rest/errors
-    */
-  private def getError(err: JsValue): String = {
-    val mainMsg = (err \ "error" \ "message").as[String]
-    val otherMsgs = (err \ "error" \ "errors").toString
-    s"PagerDuty Error: ${mainMsg}; ${otherMsgs}"
-  }
-
+class PagerDutyAPI(val authToken: String, val baseUrl: String) extends PagerDutyApiUtils {
 /**
   *  https://developer.pagerduty.com/documentation/rest/schedules/users
   */
---
+++ app/lib/pagerduty/PagerDutyApiUtils.scala
@@ -0,0 +1,41 @@
+package lib.pagerduty
+
+import play.api.libs.json._
+import play.api.libs.ws.{WSResponse, WS}
+import scala.concurrent.{ExecutionContext, Future}
+import play.api.Play.current
+import play.api.http.Status
+
+trait PagerDutyApiUtils extends Status {
+
+  require(baseUrl.startsWith("https"), "Please don't use Pager Duty without HTTPS")
+
+  val authToken: String
+  val baseUrl: String
+  val apiVersion: String = "v1"
+
+  protected def authHeader = ("Authorization" -> s"Token token=$authToken")
+
+  protected def request(path: String) = WS.url(s"$baseUrl/api/$apiVersion/$path").withHeaders(authHeader)
+
+  protected def submit(path: String, params: (String, String)*)(implicit ec: ExecutionContext) =
+    request(path).withQueryString(params: _*).execute
+
+  protected def submitJson(path: String, params: (String, String)*)(implicit ec: ExecutionContext):
+      Future[Either[String, JsValue]] =
+    submit(path, params: _*).map { response =>
+      if(response.status == OK) Right(response.json) else Left(getError(response.json))
+    }
+
+  /**
+    *  checks whether the WSResponse that was returned by PagerDuty was
+    *  an error (see:
+    *  https://developer.pagerduty.com/documentation/rest/errors
+    */
+  private def getError(err: JsValue): String = {
+    val mainMsg = (err \ "error" \ "message").as[String]
+    val otherMsgs = (err \ "error" \ "errors").toString
+    s"PagerDuty Error: ${mainMsg}; ${otherMsgs}"
+  }
+
+}
